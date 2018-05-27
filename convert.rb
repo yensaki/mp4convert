bed75bd67fa01961db2a7676cbaa6298b8992e45
saki <saki@pixiv.co.jp>
2018-05-27 11:59:22 +0900

    Convert mkv to mp4 per a dir

---
+++ convert.rb
@@ -0,0 +1,8 @@
+source_dir = ARGV[0]
+dest_dir = ARGV[1]
+
+Dir.glob("*\.mkv", base: source_dir) do |source_path|
+  filename = File.basename(source_path, ".mkv")
+  dest_path = File.join(dest_dir, "#{filename}.mp4")
+  `ffmpeg -i #{source_path} -vcodec libx264 #{dest_path}`
+end
