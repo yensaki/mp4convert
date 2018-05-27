b1380f1e9c53592259cd1132582d211dbd7aae5e
saki <saki@pixiv.co.jp>
2018-05-27 12:05:38 +0900

    Add puts log

--- convert.rb
+++ convert.rb
@@ -1,8 +1,10 @@
 source_dir = ARGV[0]
 dest_dir = ARGV[1]
 
-Dir.glob("*\.mkv", base: source_dir) do |source_path|
-  filename = File.basename(source_path, ".mkv")
+Dir.glob('*.mkv', base: source_dir) do |source_path|
+  puts "converting #{source_path}"
+  filename = File.basename(source_path, '.mkv')
   dest_path = File.join(dest_dir, "#{filename}.mp4")
   `ffmpeg -i #{source_path} -vcodec libx264 #{dest_path}`
+  puts "converted to #{dest_path}"
 end
