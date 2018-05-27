367d9ee8e98e3298061b180c84accc1baf896599
saki <saki@pixiv.co.jp>
2018-05-27 19:36:26 +0900

    Always yes with ffmpeg

--- convert.rb
+++ convert.rb
@@ -5,6 +5,6 @@ Dir.glob('*.mkv', base: source_dir) do |source_path|
   puts "converting #{source_path}"
   filename = File.basename(source_path, '.mkv')
   dest_path = File.join(dest_dir, "#{filename}.mp4")
-  `ffmpeg -i #{source_path} -vcodec libx264 #{dest_path}`
+  `ffmpeg -y -i #{source_path} -vcodec libx264 #{dest_path}`
   puts "converted to #{dest_path}"
 end
